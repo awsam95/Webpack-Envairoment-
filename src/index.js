916a9122420a6825d528ab5332a4a7284ede21ce
Awsam Barahmeh <awsam.haitham@outlook.com>
2023-06-21 15:52:17 +0300

    gitting the button to work

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import generateJoke from "./generateJoke"
 import './styles/main.scss'
 import laughing from './assets/laughing.png'
 
-const laughImg = document.getElementById('laughImg')
+const laughImg = document.getElementById('laughImg');
 laughImg.src = laughing;
 
-console.log(generateJoke())
+const jokeBtn =document.getElementById('jokebtn');
+jokeBtn.addEventListener('click', generateJoke);
+
+generateJoke();
 
